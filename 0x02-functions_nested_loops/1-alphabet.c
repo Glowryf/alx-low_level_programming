10b12b743a23fccc2af9706107ca55a5cb0042c0
Glowryf <glowryf@gmail.com>
2022-09-14 03:14:03 -0700

    a function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet - prints alphabets in lowercase.
+ *
+ * Return: 0 Always (Success)
+ *
+ */
+#include "main.h"
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++;)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
