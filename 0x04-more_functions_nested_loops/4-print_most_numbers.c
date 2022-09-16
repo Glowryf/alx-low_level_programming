8caa6f6a5f7ece1e2c5c35e673e98416c371714e
Glowryf <glowryf@gmail.com>
2022-09-15 22:16:43 -0700

    a function that prints most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+/**
+ * print_most_numbers - checks numbers 0-9
+ *
+ * Return: Always 0
+ */
+#include "main.h"
+void print_most_numbers(void)
+{
+	int g;
+
+	for (g = 0; g <= 9; g++)
+	{
+		if (g != 2)
+		{
+			if (g != 4)
+			{
+				_putchar(g);
+			}
+			_putchar('\n');
+		}
+	}
+}
