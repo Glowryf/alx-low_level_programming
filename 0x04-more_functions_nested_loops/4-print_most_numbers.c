20c5400ef350cc384399aef303610a85409ad4e1
Glowryf <glowryf@gmail.com>
2022-09-15 22:38:43 -0700

    a function that prints most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,5 @@
 /**
- * print_most_numbers - checks numbers 0-9
+ * print_most_numbers - checks numbers for a digit
  *
  * Return: Always 0
  */
@@ -8,11 +8,11 @@ void print_most_numbers(void)
 {
 	int g;
 
-	for (g = 0; g < 9; g++)
+	for (g = 48; g < 58; g++)
 	{
-		if (g != 2)
+		if (g != 50)
 		{
-			if (g != 4)
+			if (g != 52)
 			{
 				_putchar(g);
 			}
