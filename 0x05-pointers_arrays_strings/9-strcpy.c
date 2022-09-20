9613fbb6ee69b5453e064db441f61b68a3eb64aa
Glowryf <glowryf@gmail.com>
2022-09-20 02:30:35 -0700

    a function that copies a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _strcpy - a function that copies a string
+ * @src: assigned int
+ * @des: assigned int
+ * Return: dest
+ */
+#include "main.h"
+char *_strcpy(char *dest, char *src)
+{
+	int abc = 0;
+
+	while (*(src + abc) != '\0')
+	{
+		*(dest + abc) = *(src + abc);
+			abc++;
+	}
+	*(dest + abc) = '\0';
+	return (dest);
+}
