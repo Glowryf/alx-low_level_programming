7183436f2d02bf275c73b2101944026ee567af53
Glowryf <glowryf@gmail.com>
2022-09-20 02:00:47 -0700

    a function that prints an array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+/**
+ * print_array - prints n elements of an array of integer
+ * @a: assigned int
+ * @n: second assigned int
+ */
+#include "main.h"
+#include <stdio.h>
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
+}
